f6bed2dbc4eafe45045dcc0c6c78cfca9cbf8b5c
Aleksei Samoilov <lek-z@yandex.ru>
2023-09-02 14:01:30 +0200

    added some things

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,13 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
-import AppHeader  from "../app-header/app-header"
+import AppHeader from "../app-header/app-header";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 
 function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
+      <BurgerIngredients ingredients={data} />
     </div>
   );
 }
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,10 +1,72 @@
-import styles from "./app-header.module.css";
-import { data } from "../../utils/data";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./burger-ingredients.module.css";
+import {
+  Counter,
+  Tab,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
-function AppHeader() {
+const BurgerIngredients = ({ ingredients }) => {
+  const [currentTab, setCurrentTab] = useState("bun");
+
   return (
+    <section className={styles.burger_ingredietns}>
+      <div style={{ display: "flex" }}>
+        <Tab
+          value="bun"
+          active={currentTab === "bun"}
+          onClick={() => setCurrentTab("bun")}
+        >
+          Булки
+        </Tab>
+        <Tab
+          value="main"
+          active={currentTab === "main"}
+          onClick={() => setCurrentTab("main")}
+        >
+          Начинки
+        </Tab>
+        <Tab
+          value="sauce"
+          active={currentTab === "sauce"}
+          onClick={() => setCurrentTab("sauce")}
+        >
+          Соусы
+        </Tab>
+      </div>
 
+      <div className={styles.ingredients_list}>
+        {ingredients
+          .filter((ingredients) => ingredients.type === "sauce")
+          .map((item) => (
+            <article key={item._id} className="ingredient_item">
+              <img src={item.image} alt={item.name} />
+              <span className="ingredient-name">{item.name}</span>
+              <span className="ingredient-price">{item.price} ₽</span>
+              <Counter count={1} size="default" extraClass="m-1" />
+            </article>
+          ))}
+      </div>
+    </section>
   );
-}
+};
 
-export default AppHeader;
+BurgerIngredients.propTypes = {
+  ingredients: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default BurgerIngredients;
